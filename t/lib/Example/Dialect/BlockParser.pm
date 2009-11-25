e3ffdd600f4e3b73abb7b584f2bfd191194b2e64
Dave Rolsky <autarch@urth.org>
2009-11-25 16:00:58 -0600

    Add test libs for Parser tests

---
+++ t/lib/Example/Dialect/BlockParser.pm
@@ -0,0 +1,10 @@
+package Example::Dialect::BlockParser;
+
+use strict;
+use warnings;
+
+use Moose;
+
+extends 'Markdent::Dialect::Standard::BlockParser';
+
+1;
---
+++ t/lib/Example/Dialect/SpanParser.pm
@@ -0,0 +1,10 @@
+package Example::Dialect::SpanParser;
+
+use strict;
+use warnings;
+
+use Moose;
+
+extends 'Markdent::Dialect::Standard::SpanParser';
+
+1;
