c86d2293c46c8bfd85970019b6a123247d3e2adf
Dave Rolsky <autarch@urth.org>
2010-09-27 17:32:51 -0500

    Add Null handler

---
+++ lib/Markdent/Handler/Null.pm
@@ -0,0 +1,46 @@
+package Markdent::Handler::Null;
+
+use strict;
+use warnings;
+
+use namespace::autoclean;
+use Moose;
+use MooseX::StrictConstructor;
+
+with 'Markdent::Role::Handler';
+
+sub handle_event {
+    return;
+}
+
+__PACKAGE__->meta()->make_immutable();
+
+1;
+
+# ABSTRACT: A handler which ignores all events
+
+__END__
+
+=pod
+
+=head1 DESCRIPTION
+
+This class implements an event receiver which ignores all events.
+
+=head1 METHODS
+
+This class provides the following methods:
+
+=head2 Markdent::Handler::Null->new()
+
+This method creates a new handler.
+
+=head1 ROLES
+
+This class does the L<Markdent::Role::Handler> role.
+
+=head1 BUGS
+
+See L<Markdent> for bug reporting details.
+
+=cut
