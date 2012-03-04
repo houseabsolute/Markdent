518e106007af5c96b916385c467dca4f329b85d1
Dave Rolsky <autarch@urth.org>
2012-03-04 13:04:33 -0600

    Fix some bad pod links

--- lib/Markdent/Role/EventsAsMethods.pm
+++ lib/Markdent/Role/EventsAsMethods.pm
@@ -29,7 +29,8 @@ __END__
 
 =head1 DESCRIPTION
 
-This role takes a L<Markdent::Event> object and turns it into a method call.
+This role takes an object which does L<Markdent::Role::Event> role and turns
+it into a method call.
 
 =head1 METHODS
 
@@ -37,15 +38,15 @@ This role provides the following methods:
 
 =head2 $object->handle_event($event)
 
-This takes a L<Markdent::Event> object and turns it into a method call on the
-object.
+Given an object which does L<Markdent::Role::Event> role, this method makes a
+method call on C<$object> based on the event's name.
 
 The method name is the same as the value of C<< $event->event_name() >>. The
 hash reference returned by C<< $event->attributes() >> is turned into a set of
 named parameters for the method. However, any keys starting with "!" in the
 attributes will not be passed to the method.
 
-So, for example, a L<Markdown::Event::StartLink> event turns into a method
+So, for example, a L<Markdent::Event::StartLink> event turns into a method
 call like this:
 
   $handler->start_link(
