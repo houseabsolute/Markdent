eb8139967fbf06d77ca1dc83308b18c434a6565f
Dave Rolsky <autarch@urth.org>
2012-07-21 11:45:48 -0500

    Add some docs for Dialect block/span parser roles

--- lib/Markdent/Role/Dialect/BlockParser.pm
+++ lib/Markdent/Role/Dialect/BlockParser.pm
@@ -6,3 +6,20 @@ use warnings;
 use Moose::Role;
 
 1;
+
+# ABSTRACT: A role for a dialect block parser
+
+__END__
+
+=pod
+
+=head1 DESCRIPTION
+
+This role has no internals, it simply indicates that the role which consumes
+it is a block parser role.
+
+=head1 BUGS
+
+See L<Markdent> for bug reporting details.
+
+=cut
--- lib/Markdent/Role/Dialect/SpanParser.pm
+++ lib/Markdent/Role/Dialect/SpanParser.pm
@@ -6,3 +6,20 @@ use warnings;
 use Moose::Role;
 
 1;
+
+# ABSTRACT: A role for a dialect span parser
+
+__END__
+
+=pod
+
+=head1 DESCRIPTION
+
+This role has no internals, it simply indicates that the role which consumes
+it is a span parser role.
+
+=head1 BUGS
+
+See L<Markdent> for bug reporting details.
+
+=cut
